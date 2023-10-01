bc732f1a5ac8b566bc1ad0baee7b8734596d1204
qndn3tp <qndn3tp@gmail.com>
2023-10-02 00:32:19 +0900

    feat: 사용자 권한을 받는 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp( MaterialApp(
@@ -14,6 +15,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  // 권한을 받았는지 확인하는 함수
+  getPermission() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print("허락됨");
+    } else if (status.isDenied) {
+      print("거절됨");
+      Permission.contacts.request();  // 허락해달라고 팝업을 띄움
+    }
+  }
 
   var total = 3;
   var name = ["김영숙", "홍길동", "피자집"];
@@ -53,7 +65,9 @@ class _MyAppState extends State<MyApp> {
           );
         },
       ),
-      appBar: AppBar(title: Text("연락처")),
+      appBar: AppBar(title: Text("연락처"),actions: [
+        IconButton(onPressed: (){ getPermission(); }, icon: Icon(Icons.contacts))
+      ],),
       body: ListView.builder(
           itemCount: name.length,
           itemBuilder: (c, i){
